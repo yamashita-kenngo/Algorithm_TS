94ae4ad521b413d9e6b82b8b3f90dc4ad4dc5a0b
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2020-06-30 19:17:53 +0900

    タイポその他修正

--- math_puzzle/ch1/q1.ts
+++ math_puzzle/ch1/q1.ts
@@ -2,28 +2,30 @@
 10進数、2進数、8進数のいずれで表現しても回文数となる数のうち、10進数の10以上で最小の地を求める。
 */
 
-function Palindrome_Num(inputNum: number, setParam: Array<number>):boolean[]{
-    const result_Flag = setParam.map(check_radix => Check_Palindrome_OrNot(Chnage_Num_to_String(inputNum, check_radix)));
-    return result_Flag;
+function palindromeNum(inputNum: number, setParam: Array<number>): boolean[] {
+  const resultFlag = setParam.map((checkRadix) =>
+    checkPalindromeOrNot(chnageNumtoString(inputNum, checkRadix))
+  );
+  return resultFlag;
 }
 
-function Chnage_Num_to_String(inputNum: number, radix: number): string {
-    // 入力値を指定の基数に基づいて文字列に変換する
-    const targetStr = inputNum.toString(radix);
-    return targetStr;
+function chnageNumtoString(inputNum: number, radix: number): string {
+  // 入力値を指定の基数に基づいて文字列に変換する
+  const targetStr = inputNum.toString(radix);
+  return targetStr;
 }
 
-function Check_Palindrome_OrNot(inputStr: string): boolean {
-    // 入力値が回文か確認する
-    const reverse_string = Array.from(inputStr).reverse().join('');
-    return inputStr === reverse_string;
+function checkPalindromeOrNot(inputStr: string): boolean {
+  // 入力値が回文か確認する
+  const reverseString = Array.from(inputStr).reverse().join("");
+  return inputStr === reverseString;
 }
 
 let flag = true;
 let checked_Num = 10;
-while(flag){
-  flag = Palindrome_Num(checked_Num, [10, 2, 8]).includes(false);
-  checked_Num ++;
-} 
+while (flag) {
+  flag = palindromeNum(checked_Num, [10, 2, 8]).includes(false);
+  checked_Num++;
+}
 
-console.log(checked_Num -1);
+console.log(checked_Num - 1);
